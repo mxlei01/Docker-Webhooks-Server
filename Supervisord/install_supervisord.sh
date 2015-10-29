d267850024034e7123338186b7162eec0feab87e
mxlei01 <mxlei01@gmail.com>
2015-10-29 03:14:42 -0600

    Added supervisord configuration script and install scripts to install and startup the auto docker server

---
+++ Supervisord/install_supervisord.sh
@@ -0,0 +1,5 @@
+sudo apt-get install -y supervisor;
+mkdir -p /var/log/supervisor;
+cd && mv /Docker-Webhooks-Server/Supervisord /etc/supervisor/conf.d/supervisord.conf
+mkdir /var/supervisord;
+/usr/bin/supervisord;
