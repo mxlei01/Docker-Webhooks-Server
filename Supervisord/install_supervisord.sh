07f87674c1001a9047467a585997d1f7b16cf42d
mxlei01 <mxlei01@gmail.com>
2015-10-29 04:38:44 -0600

    changed supervisord location in cp

--- Supervisord/install_supervisord.sh
+++ Supervisord/install_supervisord.sh
@@ -1,5 +1,5 @@
 sudo apt-get install -y supervisor;
 sudo mkdir -p /var/log/supervisor;
-cd && sudo cp /Docker-Webhooks-Server/Supervisord/supervisord.conf /usr/etc/supervisord.conf;
+cd && sudo cp /Docker-Webhooks-Server/Supervisord/supervisord.conf /etc/supervisor/conf.d/supervisord.conf;
 sudo mkdir /var/supervisord;
 sudo /usr/bin/supervisord;
