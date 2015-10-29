23957008b9bb22d8be67c1bccf3213177b450a93
mxlei01 <mxlei01@gmail.com>
2015-10-29 00:21:16 -0600

    Added basic tests for string testing on docker_commands

--- Tornado-Application/test.py
+++ Tornado-Application/test.py
@@ -1,1 +1,34 @@
-# test.py is used to create unit tests for the tornado application
+import tornado
+import docker_commands
+from tornado.testing import AsyncTestCase
+
+class Test_APIs(AsyncTestCase):
+    @tornado.testing.gen_test
+    def test_stopRepoContainer(self):
+        # Tests the string stopRepoContainer if string replacement succeeded
+        command = docker_commands.stopRepoContainer % ("mxlei01/repo")
+        self.assertIn("mxlei01/repo", command)
+
+    @tornado.testing.gen_test
+    def test_deleteRepoContainers(self):
+        # Tests the string deleteRepoContainers if string replacement succeeded
+        command = docker_commands.deleteRepoContainers % ("mxlei01/repo")
+        self.assertIn("mxlei01/repo", command)
+
+    @tornado.testing.gen_test
+    def test_deleteRepoImages(self):
+        # Tests the string deleteRepoImages if string replacement succeeded
+        command = docker_commands.deleteRepoImages % ("mxlei01/repo")
+        self.assertIn("mxlei01/repo", command)
+
+    @tornado.testing.gen_test
+    def test_pullRepo(self):
+        # Tests the string pullRepo if string replacement succeeded
+        command = docker_commands.pullRepo % ("mxlei01/repo")
+        self.assertIn("mxlei01/repo", command)
+
+    @tornado.testing.gen_test
+    def test_runRepo(self):
+        # Tests the string runRepo if string replacement succeeded
+        command = docker_commands.runRepo % ("mxlei01/repo")
+        self.assertIn("mxlei01/repo", command)
