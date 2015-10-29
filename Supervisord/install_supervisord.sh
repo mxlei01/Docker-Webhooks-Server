a2f515e1f5123477610235ae3f98890f3288b9b3
mxlei01 <mxlei01@gmail.com>
2015-10-29 04:48:43 -0600

    Changed location again of where supervisord.conf supposed to be

--- Supervisord/install_supervisord.sh
+++ Supervisord/install_supervisord.sh
@@ -1,5 +1,5 @@
 sudo apt-get install -y supervisor;
 sudo mkdir -p /var/log/supervisor;
-cd && sudo cp Docker-Webhooks-Server/Supervisord/supervisord.conf /etc/supervisor/conf.d/supervisord.conf;
+cd && sudo cp Docker-Webhooks-Server/Supervisord/supervisord.conf /etc/supervisor/supervisord.conf;
 sudo mkdir /var/supervisord;
 sudo /usr/bin/supervisord;
