41759dfcf97ebb6b3e218756af53d91c35185b33
mxlei01 <mxlei01@gmail.com>
2015-10-29 05:11:23 -0600

    Changed port back to 8888

--- Tornado-Application/main.py
+++ Tornado-Application/main.py
@@ -7,8 +7,8 @@ from url import application
 # What this will do is listen to port 8888, and then we can access the app using
 # http://localhost:8888 on any browser, or using python requests library
 if __name__ == "__main__":
-    # Set the application to listen to port 8088
-    application.listen(8088)
+    # Set the application to listen to port 8888
+    application.listen(8888)
 
     # Get the current IOLoop
     currentIOLoop = tornado.ioloop.IOLoop.current()
