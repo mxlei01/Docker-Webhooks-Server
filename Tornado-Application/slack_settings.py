370336d12083495173022118472b728b261fc250
mxlei01 <mxlei01@gmail.com>
2015-10-29 20:33:30 -0600

    Added requestb.in for debugging

--- Tornado-Application/slack_settings.py
+++ Tornado-Application/slack_settings.py
@@ -2,5 +2,6 @@
 
 # webhook_urls contains a list of urls that the webhook should sent to
 webhook_urls = [
-    "https://hooks.slack.com/services/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"
+    "https://hooks.slack.com/services/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",
+    "http://requestb.in/t7cd5pt7"
 ]
